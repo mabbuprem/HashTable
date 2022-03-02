1c1f962b9917950d69978a06738207078b9460a1
mabbuprem <mabbupremkumar@gmail.com>
2022-03-02 07:40:40 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HashTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hash Table");
+            string sentence = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            string[] StringToArray;
+            StringToArray = sentence.Split(' ');
+            WordFrequency(StringToArray);
+        }
+
+
+        public static void WordFrequency(string[] StringToArray)
+        {
+            MyMapNode<string, int> MyMapNode = new MyMapNode<string, int>(5);
+            foreach (string word in StringToArray)
+            {
+                int count = MyMapNode.CountFrequency(word);
+                MyMapNode.Add(word, count);
+            }
+            MyMapNode.Remove("avoidable");
+            IEnumerable<string> DistinctSentence = StringToArray.Distinct<string>();
+            foreach (string word in DistinctSentence)
+            {
+                MyMapNode.Display(word);
+            }
+        }
+    }
+}
