aaa120b0ab9226690c6b1a017faa62ed2b946b87
mabbuprem <mabbupremkumar@gmail.com>
2022-03-02 07:44:49 +0530

    word frequency

--- Program.cs
+++ Program.cs
@@ -7,23 +7,20 @@ namespace HashTable
         static void Main(string[] args)
         {
             Console.WriteLine("Hash Table");
-            string sentence = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
-            string[] StringToArray;
-            StringToArray = sentence.Split(' ');
-            WordFrequency(StringToArray);
+            String[] Sentence = { "TO", "BE", "OR", "NOT", "TO", "BE" };
+            WordFrequency(Sentence);
         }
 
 
-        public static void WordFrequency(string[] StringToArray)
+        public static void WordFrequency(string[] Sentence)
         {
             MyMapNode<string, int> MyMapNode = new MyMapNode<string, int>(5);
-            foreach (string word in StringToArray)
+            foreach (string word in Sentence)
             {
                 int count = MyMapNode.CountFrequency(word);
                 MyMapNode.Add(word, count);
             }
-            MyMapNode.Remove("avoidable");
-            IEnumerable<string> DistinctSentence = StringToArray.Distinct<string>();
+            IEnumerable<string> DistinctSentence = Sentence.Distinct<string>();
             foreach (string word in DistinctSentence)
             {
                 MyMapNode.Display(word);
